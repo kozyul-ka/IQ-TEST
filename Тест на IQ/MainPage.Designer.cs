6b29fa2677f2a171e285ead8ff4de337637cbff0
Anastasia Poluectova <poluectova.an@yandex.ru>
2021-12-20 16:14:40 +0300

    Небольшая корректировка багов

--- Тест на IQ/MainPage.Designer.cs
+++ Тест на IQ/MainPage.Designer.cs
@@ -42,39 +42,43 @@ namespace Тест_на_IQ
             // 
             // Nickname_textBox
             // 
-            this.Nickname_textBox.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Nickname_textBox.Location = new System.Drawing.Point(40, 395);
+            this.Nickname_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Nickname_textBox.Location = new System.Drawing.Point(53, 486);
+            this.Nickname_textBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Nickname_textBox.Name = "Nickname_textBox";
-            this.Nickname_textBox.Size = new System.Drawing.Size(204, 30);
+            this.Nickname_textBox.Size = new System.Drawing.Size(271, 34);
             this.Nickname_textBox.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(36, 359);
+            this.label1.Location = new System.Drawing.Point(48, 442);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(177, 24);
+            this.label1.Size = new System.Drawing.Size(225, 29);
             this.label1.TabIndex = 3;
             this.label1.Text = "Введите свой ник:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Roboto", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(123)))), ((int)(((byte)(118)))));
-            this.label2.Location = new System.Drawing.Point(30, 57);
+            this.label2.Location = new System.Drawing.Point(40, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(197, 46);
+            this.label2.Size = new System.Drawing.Size(260, 54);
             this.label2.TabIndex = 4;
             this.label2.Text = "Тест на IQ";
             // 
             // label3
             // 
-            this.label3.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(36, 119);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(48, 146);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(238, 222);
+            this.label3.Size = new System.Drawing.Size(347, 274);
             this.label3.TabIndex = 6;
             this.label3.Text = resources.GetString("label3.Text");
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -90,11 +94,12 @@ namespace Тест_на_IQ
             this.btn_CheckResult.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.btn_CheckResult.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.btn_CheckResult.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_CheckResult.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_CheckResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btn_CheckResult.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btn_CheckResult.Location = new System.Drawing.Point(331, 449);
+            this.btn_CheckResult.Location = new System.Drawing.Point(441, 553);
+            this.btn_CheckResult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_CheckResult.Name = "btn_CheckResult";
-            this.btn_CheckResult.Size = new System.Drawing.Size(256, 106);
+            this.btn_CheckResult.Size = new System.Drawing.Size(341, 130);
             this.btn_CheckResult.TabIndex = 1;
             this.btn_CheckResult.UseVisualStyleBackColor = false;
             this.btn_CheckResult.Click += new System.EventHandler(this.btn_CheckResult_Click);
@@ -111,11 +116,12 @@ namespace Тест_на_IQ
             this.btn_Start.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.btn_Start.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.btn_Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Start.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btn_Start.ForeColor = System.Drawing.Color.White;
-            this.btn_Start.Location = new System.Drawing.Point(25, 449);
+            this.btn_Start.Location = new System.Drawing.Point(33, 553);
+            this.btn_Start.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Start.Name = "btn_Start";
-            this.btn_Start.Size = new System.Drawing.Size(258, 106);
+            this.btn_Start.Size = new System.Drawing.Size(344, 130);
             this.btn_Start.TabIndex = 0;
             this.btn_Start.UseVisualStyleBackColor = false;
             this.btn_Start.Click += new System.EventHandler(this.button1_Click);
@@ -126,19 +132,20 @@ namespace Тест_на_IQ
             // 
             this.pictureBox1.ImageLocation = "../.../Resources/brain.png";
             this.pictureBox1.InitialImage = global::Тест_на_IQ.Properties.Resources.brain;
-            this.pictureBox1.Location = new System.Drawing.Point(302, 45);
+            this.pictureBox1.Location = new System.Drawing.Point(403, 55);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(277, 305);
+            this.pictureBox1.Size = new System.Drawing.Size(369, 375);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // MainPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(609, 567);
+            this.ClientSize = new System.Drawing.Size(812, 698);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -147,6 +154,7 @@ namespace Тест_на_IQ
             this.Controls.Add(this.btn_Start);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainPage";
             this.Text = "MainPage";
             this.Load += new System.EventHandler(this.MainPage_Load);
